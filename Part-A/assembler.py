c4070a3cd23780e4452479ff34721ef2230d0c31
fraekta <_>
2025-06-05 13:31:39 +0800

    refactor: (assember) directives are now retained in assembled code

--- Part-A/assembler.py
+++ Part-A/assembler.py
@@ -8,8 +8,6 @@ from asm_directives import directive_map
 # read lines and convert to machine code
 
 comment = "--"
-directive_lines: list[str] = []
-instruction_lines: list[str] = []
 output_lines: list[str] = []
 initial_file = open(Path("Arch242_example_code.asm"), "r") # Change filename to final
 while (line := initial_file.readline()) != "":
@@ -20,24 +18,19 @@ while (line := initial_file.readline()) != "":
     if comment_idx != -1:
         line = line[:comment_idx]
     line.lstrip().rstrip()
+    
+    # directive
     if line[0] == ".":
-        directive_lines.append(line)
+        encoded = line
+    
+    # instruction
     else:
-        instruction_lines.append(line)
+        tokens = line.split(" ")
+        encoded = instruction_map[tokens[0]](*tokens[1:])
+        assert len(encoded) in (8, 16)
+        
+    output_lines.append(encoded)
 
-# directives first
-for line in directive_lines:
-    tokens = line.split(" ")
-    encoded = directive_map(tokens[0][1:])(*tokens[1:])
-    for machine_code in encoded:
-        assert len(machine_code) in (8, 16)
-        output_lines.append(machine_code)
-
-for line in instruction_lines:
-    tokens = line.split(" ")
-    machine_code = instruction_map[tokens[0]](*tokens[1:])
-    assert len(machine_code) in (8, 16)
-    output_lines.append(machine_code)
 
 # TODO: Implement command line arguments for filename and bin/hex
 # check if binary
